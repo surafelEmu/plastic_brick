eb4efced49740ba5515172d340965fa084ae357e
surafelEmu <surafel097@outlook.com>
2022-03-20 16:08:32 +0300

    final touches

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.onload = function () {
     
     window.onclick = function (event) {
+        var path = event.path || (event.composedPath && event.composedPath());
         if(event.target.className == 'plus') {
             console.log('plus')
 
-            event.path[3].classList.add('on')
+            path[3].classList.add('on')
         }else if(event.target.className == 'minus'){
-            event.path[3].classList.remove('on') ;
+            path[3].classList.remove('on') ;
         }
     }
 
